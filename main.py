346e351ffd131969e34d90d6611fdd003560f7f9
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-30 20:24:09 +0700

    How to Find the Index of an Element in a List

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 if __name__ == "__main__":
-  print("How to Find the Index of an Element in a List")
+  # How to Find the Index of an Element in a List
+
+  cities = ["New York", "Beijing", "Cairo", "Mumbai", "Mexico"]
+  print(cities)
+  print(cities.index("Mumbai"))
+
+  # cities.index('Osaka')
+  # ValueError
+
+  cities = ["New York", "Beijing", "Cairo", "Mumbai", "Mexico"]
+  city = "Osaka"
+
+  if city in cities:
+    index = cities.index(city)
+    print(f"The {city} has an index of {index}.")
+  else:
+    print(f"The {city} doesn't exist in the list.")
